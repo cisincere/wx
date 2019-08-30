1a4a06fd162528425c1b039a68ccf22423617954
lbb <3492359146@qq.com>
2019-08-30 09:08:07 +0800

    增加动态修改title值

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
+router.beforeEach((to, from, next) => {
+  if (to.meta.content) {
+    document.title = to.meta.title;
+  }
+  next();
+});
+
 new Vue({
   router,
   store,
--- src/router.ts
+++ src/router.ts
@@ -10,10 +10,16 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+      meta: {
+        title: 'home',
+      },
     },
     {
       path: '/about',
       name: 'about',
+      meta: {
+        title: 'about',
+      },
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
