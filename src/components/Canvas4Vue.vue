f314745f4ea3edc45051861ee6126c644cdc540d
122 <122@163.com>
2019-12-02 20:24:14 +0800

    升级vue/cli版本到4.0，添加canvas组件测试

---
+++ src/components/Canvas4Vue.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Canvas4Vue',
+  mounted() {
+  },
+  methods: {
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import Home from './views/Home.vue';
 import Reg from './components/Registered.vue';
 import Index from './views/Index.vue';
+import Canvas4Vue from './components/Canvas4Vue.vue';
 
 Vue.use(Router);
 
@@ -22,6 +23,14 @@ export default new Router({
       component: Reg,
       meta: {
         title: 'reg',
+      },
+    },
+    {
+      path: '/cv',
+      name: 'Canvas4Vue',
+      component: Canvas4Vue,
+      meta: {
+        title: 'Canvas4Vue',
       },
     },
   ],
